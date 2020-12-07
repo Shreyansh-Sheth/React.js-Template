6ba5c90941da93c92f3c1bbb365e6f646daab9cc
Shreyansh sheth <shethshreyansh777@gmail.com>
2020-12-07 20:51:52 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+//Don't change this file
+//change if you are aware of what you are doing
+
+import React from "react";
+import { render } from "react-dom";
+import Main from "./Main";
+
+const App = () => {
+  return <Main />;
+};
+
+render(<App />, document.getElementById("root"));
---
+++ src/Main.js
@@ -0,0 +1,25 @@
+//Do all Stuff Here
+//
+
+import React, { useState } from "react";
+
+
+export default function Main() {
+
+  let [name, setName] = useState("Shreyansh");
+  
+  return (
+    <div>
+      <label htmlFor="name">
+        Name:
+        <input
+          id="name"
+          placeholder="name"
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        ></input>
+      </label>
+    </div>
+  );
+}
